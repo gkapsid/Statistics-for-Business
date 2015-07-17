76e17e2188ef052a733ec996d7c739b9a6e80395
gkapsid <gkapsid@users.noreply.github.com>
2015-07-17 21:23:13 +0300

    v2

--- 01cleaning.R
+++ 01cleaning.R
@@ -2,11 +2,6 @@ mc<-read.csv("Business/Metadata_Country_v2.csv", encoding = "UTF-8")
 mi<-read.csv("Business/Metadata_Indicator_v2.csv")
 st<-read.csv("Business/st_int_rcpt_v2.csv", skip=4)
 
-#not to be used...
-#remove one row to equal the rows of two datasets
-#st1<-subset(st, st$Country.Name !="World")
-#st1$Region<-mc$Region
-
 #merge two datasets and use certain columns of the second dataset
 st1<- merge(x = st, y = mc[ , c("Country.Code", "Region")], by = "Country.Code", all.x=TRUE)
 
@@ -17,5 +12,30 @@ st1[st1$Country.Name=="Bangladesh",]$Region
 # find the region of Sint Maarten (Dutch part)
 st1[st1$Country.Name=="Sint Maarten (Dutch part)",]$Region
 
-#Video 
+#Video 2 "Preparing data before analysis"
+#Get aggregates
+Aggregates<-st1[which(st1$Region==""),]
+
+#st2 contains all countries with region not ""
+st2<-st1[which(st1$Region!=""),]
+#Get countries with insufficient data
+
+#Get all countries with less than 16 years of data (1995:2010)
+myvars <- paste("X", 1995:2010, sep="")
+newdata <- st2[myvars]
+cc<-complete.cases(newdata)
+
+#Cleaned contains countries with 16 years of data
+Cleaned<-st2[cc,]
+
+ncc<-!complete.cases(newdata)
+#Countries with insufficient or no data
+insufNoData<-st2[ncc,]
+insyears<-insufNoData[myvars]
+
+#Countries with insufficient data
+InsufData<-insufNoData[rowSums(is.na(insyears[,1:16]))<ncol(insyears),]
+#Countries with no data
+NoData<-insufNoData[rowSums(is.na(insyears[,1:16]))==ncol(insyears),]
+
 
